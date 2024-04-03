684776e615a2e1e875dc611d73695c79f7804529
GeneralKenobiego <disgreensquid@gmail.com>
2024-04-03 13:49:33 +0700

    Renamed the class file for the databases. Added an example method for getting date from the database. Adjusted all project files to the same style

--- cypress/support/ProjectApi.ts
+++ cypress/support/ProjectApi.ts
@@ -1,30 +1,30 @@
 import Chainable = Cypress.Chainable
 
 class FirstApi {
-    deleteDocument(documentId: number) {
-        cy.request('POST', '/api/documents/delete', {
-            'id': documentId.toString(),
-            'reason': 'cleaning'
-        }).then(response => {
-            expect(response.status).to.be.eq(200);
-        })
-    }
+  deleteDocument(documentId: number) {
+    cy.request('POST', '/api/documents/delete', {
+      'id': documentId.toString(),
+      'reason': 'cleaning'
+    }).then(response => {
+      expect(response.status).to.be.eq(200);
+    })
+  }
 
-    getFilteredTransactionsList(documentNumber: string) : Chainable<Object[]>{
-        return cy.request('GET', '/api/transactions/list', {
-            'documentNumber': documentNumber
-        }).then(response => {
-            expect(response.status).to.be.eq(200);
-            return response.body.items;
-        })
-    }
+  getFilteredTransactionsList(documentNumber: string) : Chainable<Object[]>{
+    return cy.request('GET', '/api/transactions/list', {
+      'documentNumber': documentNumber
+    }).then(response => {
+      expect(response.status).to.be.eq(200);
+      return response.body.items;
+    })
+  }
 }
 
 class SecondApi {
-    // Methods for a different API if needed
+  // Methods for a different API if needed
 }
 
 export class ProjectApi {
-    static first = new FirstApi();
-    static second = new SecondApi();
+  static first = new FirstApi();
+  static second = new SecondApi();
 }
--- cypress/support/RabbitMQAPI.ts
+++ cypress/support/RabbitMQAPI.ts
@@ -1,53 +1,53 @@
 class RabbitMQApi {
-    private baseUrl: string;
-    private credentialHeaders: object;
+  private baseUrl: string;
+  private credentialHeaders: object;
 
-    constructor(baseUrl: string, credentialHeaders: Object) {
-        this.baseUrl = baseUrl;
-        this.credentialHeaders = credentialHeaders;
-    }
+  constructor(baseUrl: string, credentialHeaders: Object) {
+    this.baseUrl = baseUrl;
+    this.credentialHeaders = credentialHeaders;
+  }
 
-    sendMessage(exchange: string, message: Object) {
-        return cy.request({
-            method: 'POST',
-            url: this.baseUrl + '/api/exchanges/%2F/' + exchange + '/publish',
-            headers: this.credentialHeaders,
-            body: {
-                'vhost': '/',
-                'name': exchange,
-                'properties': {
-                    'delivery_mode': 2,
-                    'headers': {
-                        'Content-Type': 'application/json',
-                    },
-                },
-                'routing_key': exchange + '.someId',
-                'delivery_mode': '2',
-                'payload': JSON.stringify(message),
-                'headers': {
-                    'Content-Type': 'application/json',
-                },
-                'props': {},
-                'payload_encoding': 'string',
+  sendMessage(exchange: string, message: Object) {
+    return cy.request({
+      method: 'POST',
+      url: this.baseUrl + '/api/exchanges/%2F/' + exchange + '/publish',
+      headers: this.credentialHeaders,
+      body: {
+        'vhost': '/',
+        'name': exchange,
+        'properties': {
+            'delivery_mode': 2,
+            'headers': {
+                'Content-Type': 'application/json',
             },
-        }).then(response => {
-            expect(response.status).to.eq(200);
-            cy.log(`Sent message to ${exchange} exchange`);
-        })
-    }
+        },
+        'routing_key': exchange + '.someId',
+        'delivery_mode': '2',
+        'payload': JSON.stringify(message),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        'props': {},
+        'payload_encoding': 'string',
+      },
+    }).then(response => {
+      expect(response.status).to.eq(200);
+      cy.log(`Sent message to ${exchange} exchange`);
+    })
+  }
 
 }
 
 export class FirstRabbitMQ {
-    private static rabbitMQBaseUrl = Cypress.env('rabbitMQUrl');
-    private static rabbitMQCredentialHeaders = {
-        'Authorization': 'Basic ' + Cypress.env('rabbitMQToken'),
-        'Content-Type': 'application/json',
-    }
+  private static rabbitMQBaseUrl = Cypress.env('rabbitMQUrl');
+  private static rabbitMQCredentialHeaders = {
+    'Authorization': 'Basic ' + Cypress.env('rabbitMQToken'),
+    'Content-Type': 'application/json',
+  }
 
-    static api = new RabbitMQApi(this.rabbitMQBaseUrl, this.rabbitMQCredentialHeaders);
+  static api = new RabbitMQApi(this.rabbitMQBaseUrl, this.rabbitMQCredentialHeaders);
 }
 
 export class SecondRabbitMQ {
-    // Config for a different RabbitMQ if needed
+  // Config for a different RabbitMQ if needed
 }
